a7fbe1e5794028392e03897ec11272dba99c436f
Gregorio Perez <jose.gre1222@gmail.com>
2020-11-08 17:11:32 -0500

    estructura

---
+++ src/co/ufps/model/Empleado.java
@@ -0,0 +1,85 @@
+package co.ufps.model;
+
+import java.io.Serializable;
+import javax.persistence.*;
+import java.util.Date;
+
+
+/**
+ * The persistent class for the empleado database table.
+ * 
+ */
+@Entity
+@NamedQuery(name="Empleado.findAll", query="SELECT e FROM Empleado e")
+public class Empleado implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private String codigo;
+
+	private String cedula;
+
+	@Temporal(TemporalType.DATE)
+	private Date fechaingreso;
+
+	@Temporal(TemporalType.DATE)
+	private Date fechanacimiento;
+
+	@Temporal(TemporalType.DATE)
+	private Date fecharetiro;
+
+	private String nombre;
+
+	public Empleado() {
+	}
+
+	public String getCodigo() {
+		return this.codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getCedula() {
+		return this.cedula;
+	}
+
+	public void setCedula(String cedula) {
+		this.cedula = cedula;
+	}
+
+	public Date getFechaingreso() {
+		return this.fechaingreso;
+	}
+
+	public void setFechaingreso(Date fechaingreso) {
+		this.fechaingreso = fechaingreso;
+	}
+
+	public Date getFechanacimiento() {
+		return this.fechanacimiento;
+	}
+
+	public void setFechanacimiento(Date fechanacimiento) {
+		this.fechanacimiento = fechanacimiento;
+	}
+
+	public Date getFecharetiro() {
+		return this.fecharetiro;
+	}
+
+	public void setFecharetiro(Date fecharetiro) {
+		this.fecharetiro = fecharetiro;
+	}
+
+	public String getNombre() {
+		return this.nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+}
